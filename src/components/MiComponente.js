be61a599f7eca2636c810f72bb8f7c1d37f9b42f
Dario Vergara Zatti <dvergarazatti@uade.edu.ar>
2020-08-09 15:07:23 -0300

    Agregamos el maldito menu

---
+++ src/components/MiComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MiComponente extends React.Component {
+
+    render () {
+        return (
+           <h1>"Mi componente"</h1>     
+        )
+    };
+
+}
+export default MiComponente;
---
+++ src/components/SimpleMenu.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+/*
+function useState<S>(initialState: (() => S) | S) {
+    const dispatcher = resolveDispatcher();
+    return dispatcher.useState(initialState);
+  }
+*/
+
+export default function SimpleMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(0);
+
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                Open Menu
+      </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+//export default SimpleMenu(); 
