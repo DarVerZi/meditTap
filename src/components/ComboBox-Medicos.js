155bef30407e4113f162dd00b3aba2091cd021e7
GCamin <gian-1998@hotmail.com>
2020-08-13 01:27:36 -0300

    componente combobox-medicos-turnos

---
+++ src/components/ComboBox-Medicos.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+export default function ComboBox() {
+  return (
+    <Autocomplete
+      id="combo-box-demo"
+      options={top100Films}
+      getOptionLabel={(option) => option.title}
+      style={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Medico" variant="outlined" />}
+    />
+  );
+}
+
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { title: 'Dr. Gomez'},
+  { title: 'Dr. Rodriguez'},
+  { title: 'Dr. Lopez'},
+  { title: 'Dra. Gimenez'},
+  { title: 'Dra. Martinez'},
+];
